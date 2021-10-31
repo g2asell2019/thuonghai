337ffd8169dbcfd259e7bbfc82872db2e3020299
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2021-10-31 18:17:43 +0700

    Create install.sh
    install everything

---
+++ katacoda/course6/install.sh
@@ -0,0 +1,9 @@
+wget -O download.sh https://raw.githubusercontent.com/g2asell2019/thuonghai/master/katacoda/course6/download.sh
+
+wget -O exit.sh https://raw.githubusercontent.com/g2asell2019/thuonghai/master/katacoda/course6/exit.sh
+
+wget -O main.sh https://raw.githubusercontent.com/g2asell2019/thuonghai/master/katacoda/course6/main.sh
+
+wget -O noVNC.sh https://raw.githubusercontent.com/g2asell2019/thuonghai/master/katacoda/course6/noVNC.sh
+
+wget -O upload.sh https://raw.githubusercontent.com/g2asell2019/thuonghai/master/katacoda/course6/download.sh
